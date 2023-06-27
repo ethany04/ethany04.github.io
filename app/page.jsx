df12fa1fc2bc494a21d74cf196ebfb196b94f45d
ethany04 <ethan.yu@utexas.edu>
2023-06-26 19:47:23 -0500

    removed project directory

--- app/page.jsx
+++ app/page.jsx
@@ -60,7 +60,7 @@ const Projects = () => {
         </div>
         <div>
           {projectImages.map((project) => (
-            <Link href={`/projects/${project[3]}`}>
+            <Link href={`/${project[3]}`}>
               <ProjectCard
                 title={project[0]}
                 image={project[1]}
